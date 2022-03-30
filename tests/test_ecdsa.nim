2953f6426ffa977ca4badfebc244ee6190e0e5c0
frodo821 <sakaic2003@gmail.com>
2022-03-31 01:57:49 +0900

    add test case test case "derive public key from signed message with parity" might be fail and commented out for now

--- tests/test_ecdsa.nim
+++ tests/test_ecdsa.nim
@@ -49,3 +49,17 @@ suite "generate key pair":
 
     test "derive public key from private key":
         check(curve.derivePubKeyFromSecret(prikey) == pubkey)
+
+suite "sign message and find public key":
+    setup:
+        let prikey = 0xc0dec0dec0dec0dec0dec0dec0dec0dec0dec0dec0dec0dec0dec0dec0dec0de'bi #'
+        let pubkey = 0x44643bb6b393ac20a6175c713175734a72517c63d6f73a3ca90a15356f2e967da03d16431441c61ac69aeabb7937d333829d9da50431ff6af38536aa262497b27'bi #'
+        let curve: Curve = secp256k1.secp256k1
+        let message = "Hello, Nim-lang!!"
+
+    test "check ecdsa signing and verifying":
+        check(curve.verifySignature(pubkey, message, curve.createSignature(prikey, message)))
+
+    # test "derive public key from signed message with parity":
+    #     check:
+    #         curve.toUncompressed(curve.recoverPubKey(message, curve.createSignature(prikey, message))) == pubkey
